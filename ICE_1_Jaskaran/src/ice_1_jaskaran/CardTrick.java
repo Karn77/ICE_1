dd735a559437429a8c9d4765da5fef228707a471
Karn77 <102419132+Karn77@users.noreply.github.com>
2025-06-02 22:12:03 -0400

    Replaced user input with hard-coded lucky card

--- ICE_1_Jaskaran/src/ice_1_jaskaran/CardTrick.java
+++ ICE_1_Jaskaran/src/ice_1_jaskaran/CardTrick.java
@@ -9,35 +9,42 @@ package ice_1_jaskaran;
  * @author JASKARAN SINGH
  */
 
+
+
 import java.util.Random;
 
 public class CardTrick {
     public static void main(String[] args) {
 
-        
         Card[] magicHand = new Card[7];
 
         // Define possible suits
         String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
         Random rand = new Random();
 
-        // Fill the array with random cards
+        
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            c.setValue(rand.nextInt(13) + 1);
-            c.setSuit(suits[rand.nextInt(4)]); 
+            c.setValue(rand.nextInt(13) + 1); // values 1-13
+            c.setSuit(suits[rand.nextInt(suits.length)]);
             magicHand[i] = c;
         }
 
-        
+      
         Card luckyCard = new Card();
-        luckyCard.setValue(7);
-        luckyCard.setSuit("Hearts"); 
+        luckyCard.setValue(7);        
+        luckyCard.setSuit("Hearts");    
 
        
+        System.out.println("Magic Hand:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
+
         boolean found = false;
         for (Card card : magicHand) {
-            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+            if (card.getValue() == luckyCard.getValue() &&
+                card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
                 found = true;
                 break;
             }
@@ -45,9 +52,11 @@ public class CardTrick {
 
       
         if (found) {
-            System.out.println("You win! The lucky card (" + luckyCard.getValue() + " of " + luckyCard.getSuit() + ") was found.");
+            System.out.println("You win! The lucky card (" +
+                luckyCard.getValue() + " of " + luckyCard.getSuit() + ") was found.");
         } else {
-            System.out.println("Sorry, you lose. The lucky card (" + luckyCard.getValue() + " of " + luckyCard.getSuit() + ") was not found.");
+            System.out.println("Sorry, you lose. The lucky card (" +
+                luckyCard.getValue() + " of " + luckyCard.getSuit() + ") was not found.");
         }
     }
 }
